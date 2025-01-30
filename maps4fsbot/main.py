31f310520b7bb041421c557c0955d231d0e87c72
Stan Soldatov <iwatkot@gmail.com>
2025-01-30 22:50:14 +0100

    Basic features.

--- maps4fsbot/main.py
+++ maps4fsbot/main.py
@@ -1,38 +1,84 @@
 """Main file for the bot."""
 
 import discord
+from discord.ext import commands
+from discord.ext.commands.context import Context
 
 from maps4fsbot.config import DISCORD_TOKEN
 from maps4fsbot.templates import Messages
 from maps4fsbot.triggers.messages.message_base import MessageTrigger
 
-
-class MyClient(discord.Client):
-    """Custom client for the bot."""
-
-    async def on_message(self, message: discord.Message) -> None:
-        """Event handler for when a message is sent in a channel the bot can see.
-
-        Arguments:
-            message (discord.Message): The message that was sent in a channel the bot can see.
-        """
-        response = MessageTrigger.get_response(message)
-        if response:
-            await message.channel.send(f"{message.author.mention} {response}")
-
-    async def on_member_join(self, member: discord.Member) -> None:
-        """Event handler for when a new member joins the server.
-
-        Arguments:
-            member (discord.Member): The member that joined the server.
-        """
-        channel = discord.utils.get(member.guild.text_channels, name="welcome")
-        if channel:
-            await channel.send(f"{member.mention} {Messages.welcome}")
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)  # type: ignore
-client.run(DISCORD_TOKEN)  # type: ignore
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+
+@bot.event
+async def on_message(message: discord.Message) -> None:
+    """Event handler for when a message is sent in a channel the bot can see.
+
+    Arguments:
+        message (discord.Message): The message that was sent in a channel the bot can see.
+    """
+    response = MessageTrigger.get_response(message)
+    if response:
+        await message.channel.send(f"{message.author.mention} {response}")
+    await bot.process_commands(message)
+
+
+@bot.event
+async def on_member_join(member: discord.Member) -> None:
+    """Event handler for when a new member joins the server.
+
+    Arguments:
+        member (discord.Member): The member that joined the server.
+    """
+    channel = discord.utils.get(member.guild.text_channels, name="welcome")
+    if channel:
+        await channel.send(f"{member.mention} {Messages.welcome}")
+
+
+@bot.command()
+async def docs(ctx: Context) -> None:
+    """Sends a link to the documentation."""
+    await ctx.send(Messages.docs)
+
+
+@bot.command()
+async def schema(ctx: Context) -> None:
+    """Sends a link to the texture schema."""
+    await ctx.send(Messages.schema)
+
+
+@bot.command()
+async def settings(ctx: Context) -> None:
+    """Sends a link to the advanced settings."""
+    await ctx.send(Messages.settings)
+
+
+@bot.command()
+async def expert(ctx: Context) -> None:
+    """Sends a link to the expert settings."""
+    await ctx.send(Messages.expert)
+
+
+@bot.command()
+async def debugge(ctx: Context) -> None:
+    """Sends a link to the instructions for debugging Giants Editor."""
+    await ctx.send(Messages.debugge)
+
+
+@bot.command()
+async def debuggame(ctx: Context) -> None:
+    """Sends a link to the instructions for debugging the Farming Simulator game."""
+    await ctx.send(Messages.debuggame)
+
+
+@bot.command()
+async def structure(ctx: Context) -> None:
+    """Sends a link to the instructions for correct file structure."""
+    await ctx.send(Messages.structure)
+
+
+bot.run(DISCORD_TOKEN)  # type: ignore
--- maps4fsbot/templates.py
+++ maps4fsbot/templates.py
@@ -1,5 +1,46 @@
+"""Templates for the bot messages."""
+
+
 class Messages:
+    """Templates for the bot messages."""
+
     welcome = (
-        "welcome to the maps4fs Dicord server! Make sure that you have read the [docs](<https://"
+        "welcome to the maps4fs Dicord server!\nMake sure that you have read the [docs](<https://"
         "github.com/iwatkot/maps4fs/tree/main/docs>) section before asking any questions."
+    )
+
+    docs = (
+        "Here's a link to the [docs](<https://github.com/iwatkot/maps4fs/tree/main/docs>) section."
+    )
+
+    schema = (
+        "Here's a link to the [texture schema](<https://github.com/iwatkot/maps4fs/blob/main/"
+        "README.md#Texture-schema>) explanation."
+    )
+
+    settings = (
+        "Here's a link to the [advanced settings](<https://github.com/iwatkot/maps4fs/blob/main/"
+        "README.md#Advanced-settings>) of the generator."
+    )
+
+    expert = (
+        "Here's a link to the [expert settings](<https://github.com/iwatkot/maps4fs/blob/main/"
+        "README.md#Expert-settings>) of the generator."
+    )
+
+    debugge = (
+        "Here's a link to the [debugging](<https://github.com/iwatkot/maps4fs/blob/main/docs/"
+        "FAQ.md#giants-editor-crashes-when-i-try-to-open-the-map-what-should-i-do>) instructions "
+        "for Giants Editor."
+    )
+
+    debuggame = (
+        "Here's a link to the [debugging](<https://github.com/iwatkot/maps4fs/blob/main/docs/"
+        "FAQ.md#game-is-crashing-or-hangs-when-i-try-to-load-the-map-what-should-i-do>) "
+        "instructions for the Farming Simulator game."
+    )
+
+    structure = (
+        "Here's a link to the [correct file structure](<https://github.com/iwatkot/maps4fs/blob/"
+        "main/docs/FAQ.md#the-game-cant-see-the-map-mod-what-should-i-do>) for the mod archive."
     )
